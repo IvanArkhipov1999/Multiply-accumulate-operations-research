1e32f834d45fff6ced5ebc5735937c3ee3ee5014
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2022-07-02 13:25:02 +0300

    add msub time measure

---
+++ msub/time_measure/matrix/matrix_clang_with_msub.s
@@ -0,0 +1,174 @@
+	.text
+	.abicalls
+	.option	pic0
+	.section	.mdebug.abi32,"",@progbits
+	.nan	legacy
+	.module	fp=xx
+	.module	nooddspreg
+	.file	"matrix.c"
+	.text
+	.globl	main                    # -- Begin function main
+	.p2align	2
+	.type	main,@function
+	.set	nomicromips
+	.set	nomips16
+	.ent	main
+main:                                   # @main
+	.frame	$fp,480032,$ra
+	.mask 	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	.set	noat
+# %bb.0:
+	lui	$1, 7
+	addiu	$1, $1, 21280
+	subu	$sp, $sp, $1
+	lui	$1, 7
+	addu	$1, $sp, $1
+	sw	$ra, 21276($1)          # 4-byte Folded Spill
+	lui	$1, 7
+	addu	$1, $sp, $1
+	sw	$fp, 21272($1)          # 4-byte Folded Spill
+	move	$fp, $sp
+	lui	$1, 5
+	addiu	$2, $zero, 0
+	addiu	$4, $zero, 800
+	addiu	$5, $zero, 200
+	addu	$1, $fp, $1
+	addiu	$3, $1, -7656
+$BB0_1:                                 # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_2 Depth 2
+	addiu	$6, $zero, 0
+	addiu	$7, $zero, 0
+$BB0_2:                                 #   Parent Loop BB0_1 Depth=1
+                                        # =>  This Inner Loop Header: Depth=2
+	addu	$1, $3, $7
+	addiu	$7, $7, 4
+	sw	$6, 0($1)
+	bne	$7, $4, $BB0_2
+	addu	$6, $6, $2
+# %bb.3:                                #   in Loop: Header=BB0_1 Depth=1
+	addiu	$2, $2, 1
+	bne	$2, $5, $BB0_1
+	addiu	$3, $3, 800
+# %bb.4:
+	lui	$1, 2
+	addiu	$2, $zero, 0
+	addiu	$4, $zero, 200
+	addu	$1, $fp, $1
+	addiu	$3, $1, 28952
+$BB0_5:                                 # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_6 Depth 2
+	move	$5, $3
+	addiu	$6, $zero, 0
+$BB0_6:                                 #   Parent Loop BB0_5 Depth=1
+                                        # =>  This Inner Loop Header: Depth=2
+	addu	$1, $2, $6
+	addiu	$6, $6, 1
+	sw	$1, 0($5)
+	bne	$6, $4, $BB0_6
+	addiu	$5, $5, 4
+# %bb.7:                                #   in Loop: Header=BB0_5 Depth=1
+	addiu	$2, $2, 1
+	bne	$2, $4, $BB0_5
+	addiu	$3, $3, 800
+# %bb.8:
+	lui	$1, 5
+	addiu	$2, $zero, 0
+	addiu	$5, $fp, 24
+	addiu	$6, $zero, 800
+	addiu	$7, $zero, 200
+	addiu	$8, $zero, 2000
+	addu	$1, $fp, $1
+	addiu	$3, $1, -7656
+	lui	$1, 2
+	addu	$1, $fp, $1
+	addiu	$4, $1, 28952
+$BB0_9:                                 # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_10 Depth 2
+                                        #       Child Loop BB0_11 Depth 3
+                                        #         Child Loop BB0_12 Depth 4
+	move	$9, $3
+	addiu	$10, $zero, 0
+$BB0_10:                                #   Parent Loop BB0_9 Depth=1
+                                        # =>  This Loop Header: Depth=2
+                                        #       Child Loop BB0_11 Depth 3
+                                        #         Child Loop BB0_12 Depth 4
+	addiu	$11, $zero, 0
+	move	$12, $4
+$BB0_11:                                #   Parent Loop BB0_9 Depth=1
+                                        #     Parent Loop BB0_10 Depth=2
+                                        # =>    This Loop Header: Depth=3
+                                        #         Child Loop BB0_12 Depth 4
+	sll	$1, $10, 5
+	sll	$13, $10, 8
+	move	$14, $12
+	addiu	$24, $zero, 0
+	addiu	$15, $zero, 0
+	subu	$1, $13, $1
+	sll	$13, $10, 10
+	subu	$1, $13, $1
+	sll	$13, $11, 2
+	addu	$1, $5, $1
+	addu	$13, $1, $13
+	sw	$zero, 0($13)
+	mtlo    $zero
+$BB0_12:                                #   Parent Loop BB0_9 Depth=1
+                                        #     Parent Loop BB0_10 Depth=2
+                                        #       Parent Loop BB0_11 Depth=3
+                                        # =>      This Inner Loop Header: Depth=4
+	addu	$1, $9, $24
+	lw	$25, 0($14)
+	addiu	$14, $14, 800
+	addiu	$24, $24, 4
+	lw	$1, 0($1)
+	bne	$24, $6, $BB0_12
+	msub	$1, $25
+# %bb.13:                               #   in Loop: Header=BB0_11 Depth=3
+	mflo    $15
+	addiu	$12, $12, 4
+	addiu	$11, $11, 1
+	bne	$11, $7, $BB0_11
+	sw	$15, 0($13)
+# %bb.14:                               #   in Loop: Header=BB0_10 Depth=2
+	addiu	$10, $10, 1
+	bne	$10, $7, $BB0_10
+	addiu	$9, $9, 800
+# %bb.15:                               #   in Loop: Header=BB0_9 Depth=1
+	addiu	$2, $2, 1
+	bne	$2, $8, $BB0_9
+	nop
+# %bb.16:
+	lui	$1, %hi($.str)
+	lw	$5, 24($fp)
+	jal	printf
+	addiu	$4, $1, %lo($.str)
+	move	$sp, $fp
+	lui	$1, 7
+	addu	$1, $sp, $1
+	lw	$fp, 21272($1)          # 4-byte Folded Reload
+	lui	$1, 7
+	addu	$1, $sp, $1
+	lw	$ra, 21276($1)          # 4-byte Folded Reload
+	lui	$1, 7
+	addiu	$1, $1, 21280
+	jr	$ra
+	addu	$sp, $sp, $1
+	.set	at
+	.set	macro
+	.set	reorder
+	.end	main
+$func_end0:
+	.size	main, ($func_end0)-main
+                                        # -- End function
+	.type	$.str,@object           # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+$.str:
+	.asciz	"%i\n"
+	.size	$.str, 4
+
+
+	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
+	.section	".note.GNU-stack","",@progbits
+	.text
---
+++ msub/time_measure/matrix_reduce3/matrix_gcc_without_msub.s
@@ -0,0 +1,162 @@
+	.file	1 "matrix.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.module	fp=xx
+	.module	nooddspreg
+	.abicalls
+	.text
+	.section	.rodata.str1.4,"aMS",@progbits,1
+	.align	2
+$LC0:
+	.ascii	"%i\012\000"
+	.section	.text.startup,"ax",@progbits
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$sp,480040,$31		# vars= 480008, regs= 1/0, args= 16, gp= 8
+	.mask	0x80000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$sp,$sp,-32752
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	li	$3,393216			# 0x60000
+	li	$6,458752			# 0x70000
+	sw	$31,32748($sp)
+	ori	$3,$3,0xd338
+	lw	$15,%got(__stack_chk_guard)($28)
+	subu	$sp,$sp,$3
+	ori	$2,$6,0x5320
+	li	$12,-524288			# 0xfffffffffff80000
+	addu	$2,$2,$sp
+	lw	$4,0($15)
+	ori	$6,$6,0x531c
+	.cprestore	16
+	ori	$12,$12,0xacfc
+	addu	$6,$6,$sp
+	addu	$12,$2,$12
+	move	$3,$0
+	li	$5,200			# 0xc8
+	sw	$4,0($6)
+	move	$2,$12
+	addiu	$6,$2,800
+$L23:
+	move	$4,$0
+$L3:
+	sw	$4,0($2)
+	addiu	$2,$2,4
+	bne	$2,$6,$L3
+	addu	$4,$4,$3
+
+	addiu	$3,$3,1
+	bne	$3,$5,$L23
+	addiu	$6,$2,800
+
+	li	$2,458752			# 0x70000
+	li	$14,-327680			# 0xfffffffffffb0000
+	ori	$2,$2,0x5320
+	addu	$2,$2,$sp
+	addiu	$14,$14,7676
+	move	$3,$0
+	addu	$14,$2,$14
+	li	$5,200			# 0xc8
+	move	$2,$14
+	addiu	$6,$2,800
+$L24:
+	move	$4,$3
+$L5:
+	sw	$4,0($2)
+	addiu	$2,$2,4
+	bne	$2,$6,$L5
+	addiu	$4,$4,1
+
+	addiu	$3,$3,1
+	bne	$3,$5,$L24
+	addiu	$6,$2,800
+
+	li	$2,458752			# 0x70000
+	li	$13,-196608			# 0xfffffffffffd0000
+	ori	$2,$2,0x5320
+	addu	$2,$2,$sp
+	li	$9,131072			# 0x20000
+	ori	$13,$13,0x8efc
+	addu	$13,$2,$13
+	addiu	$9,$9,28928
+	li	$24,2000			# 0x7d0
+	addiu	$13,$13,800
+	addu	$25,$12,$9
+	move	$11,$13
+$L25:
+	move	$10,$12
+$L12:
+	addiu	$7,$11,-800
+	move	$8,$14
+$L9:
+	addiu   $16, $zero, 0
+	addiu   $4, $zero, 0
+	addu	$6,$8,$9
+	move	$2,$8
+	move	$3,$10
+$L8:
+	addiu	$2,$2,800
+	#lw	$5,0($3)
+	#lw	$4,-800($2)
+	#addiu	$3,$3,4
+	mul     $4, $4, $5
+	bne	$6,$2,$L8
+	sub     $16, $16, $4
+
+	addiu	$7,$7,4
+	addiu	$8,$8,4
+	bne	$7,$11,$L9
+	sw	$16,-4($7)
+
+	addiu	$10,$10,800
+	bne	$10,$25,$L12
+	addiu	$11,$7,800
+
+	addiu	$24,$24,-1
+	bne	$24,$0,$L25
+	move	$11,$13
+
+	li	$2,458752			# 0x70000
+	ori	$2,$2,0x531c
+	addu	$2,$2,$sp
+	lw	$3,0($2)
+	lw	$2,0($15)
+	bne	$3,$2,$L22
+	li	$3,458752			# 0x70000
+
+	lw	$25,%call16(printf)($28)
+	li	$2,-131072			# 0xfffffffffffe0000
+	ori	$3,$3,0x5320
+	addu	$3,$3,$sp
+	li	$8,393216			# 0x60000
+	addu	$2,$3,$2
+	ori	$8,$8,0xd338
+	lui	$4,%hi($LC0)
+	lw	$5,-28932($2)
+	addu	$sp,$sp,$8
+	addiu	$4,$4,%lo($LC0)
+	lw	$31,32748($sp)
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jr	$25
+	addiu	$sp,$sp,32752
+
+$L22:
+	lw	$25,%call16(__stack_chk_fail)($28)
+	.reloc	1f,R_MIPS_JALR,__stack_chk_fail
+1:	jalr	$25
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
