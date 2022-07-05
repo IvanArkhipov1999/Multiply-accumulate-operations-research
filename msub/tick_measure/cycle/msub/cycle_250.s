296021b3379a7838e5a767f4f411d046e860a57b
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2022-07-05 10:54:04 +0300

    add msub cycle

---
+++ msub/tick_measure/cycle/msub/cycle_250.s
@@ -0,0 +1,88 @@
+	.file	1 "tick_counter2.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.module	fp=xx
+	.module	nooddspreg
+	.abicalls
+	.text
+	.rdata
+	.align	2
+$LC0:
+	.ascii	"%u\012\000"
+	.text
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,48,$31		# vars= 16, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-48
+	sw	$31,44($sp)
+	sw	$fp,40($sp)
+	move	$fp,$sp
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	.cprestore	16
+	sw	$0,28($fp)
+	.option	pic0
+	b	$L2
+	nop
+
+	.option	pic2
+$L3:
+	addi	$10, $0, 1
+	addi	$11, $0, 1
+	addi	$12, $0, 0
+	addi	$13, $0, 250
+
+# fix start
+	rdhwr	$2, $2
+# some code
+$A1:
+	addiu	$12, $12, 1
+	bne	$12, $13, $A1
+	msub	$10, $11
+# fix end
+	rdhwr	$3, $2
+# subtraction
+	subu	$2,$3,$2
+# print
+	move	$5,$2
+	lui	$2,%hi($LC0)
+	addiu	$4,$2,%lo($LC0)
+	lw	$2,%call16(printf)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	lw	$2,28($fp)
+	addiu	$2,$2,1
+	sw	$2,28($fp)
+$L2:
+	lw	$2,28($fp)
+	slt	$2,$2,100
+	bne	$2,$0,$L3
+	nop
+
+	nop
+	move	$sp,$fp
+	lw	$31,44($sp)
+	lw	$fp,40($sp)
+	addiu	$sp,$sp,48
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
